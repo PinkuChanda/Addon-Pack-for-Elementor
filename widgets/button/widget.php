54d32a5cea45c43e8ed11d58c13821cfd0c8c9a4
Pinku Chanda <pinkuchanda654@gmail.com>
2020-04-29 22:37:38 +0600

    Update fetures and added existing page link in Button Widget.

--- widgets/button/widget.php
+++ widgets/button/widget.php
@@ -1,17 +1,24 @@
 <?php
 namespace AddonPack\Elementor\Widget;
 
-use \Elementor\Group_Control_Box_Shadow;
 use Elementor\Widget_Base;
+use AddonPack\Includes;
 use Elementor\Controls_Manager;
 use Elementor\Group_Control_Background;
 use Elementor\Group_Control_Typography;
-use \Elementor\Scheme_Typography;
+use Elementor\Group_Control_Box_Shadow;
+use Elementor\Scheme_Typography;
 
 
 if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
 
 class Button extends Widget_Base {
+
+	protected $templateInstance;
+
+    public function getPostsInstance(){
+        return $this->templateInstance = Includes\AddonPack_Helper::getInstance();
+    }
 
 	public function get_name() {
 		return 'ap-button';
@@ -47,21 +54,50 @@ class Button extends Widget_Base {
 			[
 				'label' => __( 'Text', 'addon-pack' ),
 				'type' => Controls_Manager::TEXT,
-				'default' => __('Addon Pack', 'addon-pack'),
+				'default' => __('With Addon Pack', 'addon-pack'),
 				'placeholder' => __( 'Enter Your Text', 'addon-pack' ),
 				'label_block'   => true,
 			]
 		);
 
-		$this->add_control(
-			'ap_button_url',
+		$this->add_control('ap_button_link_selection', 
 			[
-				'label'       => esc_html__( 'Link URL', 'addon-pack' ),
-				'type'        => Controls_Manager::URL,
-				'placeholder' => esc_html__( 'Enter Your Vaild URL', 'addon-pack' ),
-				'default'     => [
-					'url' => '#',
+				'label'         => __('Link Type', 'addon-pack'),
+				'type'          => Controls_Manager::SELECT,
+				'options'       => [
+					'custom_url'   => __('URL', 'addon-pack'),
+					'existing_url'  => __('Existing Page', 'addon-pack'),
 				],
+				'default'       => 'custom_url',
+				'label_block'   => true,
+			]
+		);
+
+		$this->add_control('ap_button_url',
+			[
+				'label'         => __('Link', 'addon-pack'),
+				'type'          => Controls_Manager::URL,
+				'dynamic'       => [ 'active' => true ],
+				'default'       => [
+					'url'   => '#',
+				],
+				'label_block'   => true,
+				'condition'     => [
+					'ap_button_link_selection' => 'custom_url'
+				]
+			]
+		);
+
+		$this->add_control('ap_button_existing_url',
+			[
+				'label'         => __('Existing Page', 'addon-pack'),
+				'type'          => Controls_Manager::SELECT2,
+				'options'       => $this->getPostsInstance()->get_all_posts(),
+				'condition'     => [
+					'ap_button_link_selection'     => 'existing_url',
+				],
+				'multiple'      => false,
+				'label_block'   => true,
 			]
 		);
 
@@ -86,8 +122,9 @@ class Button extends Widget_Base {
 			[
 				'label'       => esc_html__( 'Icon', 'addon-pack' ),
 				'type'        => Controls_Manager::ICON,
-				'label_block' => true,
+				'default' => 'fa fa-heart',
 				'separator'   => 'before',
+				'label_block' => true,
 			]
 		);
 
@@ -118,9 +155,9 @@ class Button extends Widget_Base {
 						'max' => 50,
 					],
 				],
-					'default' => [
-						'size' => 8,
-					],
+				'default' => [
+					'size' => 5,
+				],
 				'condition' => [
 					'ap_button_icon!' => '',
 				],
@@ -191,6 +228,20 @@ class Button extends Widget_Base {
 			]
 		);
 
+		$this->add_control(
+			'ap_button_icon_color',
+			[
+				'label'     => esc_html__( 'Icon Color', 'addon-pack' ),
+				'type'      => Controls_Manager::COLOR,
+				'selectors' => [
+					'{{WRAPPER}} .ap-button i' => 'color: {{VALUE}};',
+				],
+				'condition' => [
+					'ap_button_icon!' => ''
+				]
+			]
+		);
+
 		$this->add_group_control(
 			Group_Control_Background::get_type(),
 			[
@@ -199,13 +250,14 @@ class Button extends Widget_Base {
 				'selector'  => '{{WRAPPER}} .ap-button',
 			]
 		);
+		
 
 		$this->add_control(
 			'ap_button_border_style',
 			[
 				'label'   => esc_html__( 'Border Style', 'addon-pack' ),
 				'type'    => Controls_Manager::SELECT,
-				'default' => 'solid',
+				'default' => 'none',
 				'options' => [
 					'none'   => esc_html__( 'None', 'addon-pack' ),
 					'solid'  => esc_html__( 'Solid', 'addon-pack' ),
@@ -226,12 +278,6 @@ class Button extends Widget_Base {
 				'label' => esc_html__( 'Border Size', 'addon-pack' ),
 				'type'  => Controls_Manager::DIMENSIONS,
 				'size_units' => [ 'px', '%' ],
-				'default' => [
-					'top'    => 3,
-					'right'  => 3,
-					'bottom' => 3,
-					'left'   => 3,
-				],
 				'selectors'  => [
 					'{{WRAPPER}} .ap-button' => 'border-width: {{TOP}}{{UNIT}} {{RIGHT}}{{UNIT}} {{BOTTOM}}{{UNIT}} {{LEFT}}{{UNIT}};',
 				],
@@ -246,7 +292,6 @@ class Button extends Widget_Base {
 			[
 				'label'     => esc_html__( 'Border Color', 'addon-pack' ),
 				'type'      => Controls_Manager::COLOR,
-				'default'   => '#666',
 				'selectors' => [
 					'{{WRAPPER}} .ap-button' => 'border-color: {{VALUE}};',
 				],
@@ -293,7 +338,7 @@ class Button extends Widget_Base {
 			Group_Control_Typography::get_type(),
 			[
 				'name'     => 'ap_button_typography',
-				'scheme'   => Scheme_Typography::TYPOGRAPHY_1,
+				'scheme'   => Scheme_Typography::TYPOGRAPHY_4,
 				'selector' => '{{WRAPPER}} .ap-button .ap-button-text',
 			]
 		);
@@ -313,8 +358,22 @@ class Button extends Widget_Base {
 				'label'     => esc_html__( 'Text Color', 'addon-pack' ),
 				'type'      => Controls_Manager::COLOR,
 				'selectors' => [
-					'{{WRAPPER}} .ap-button:hover' => 'color: {{VALUE}};',
+					'{{WRAPPER}} .ap-button:hover .ap-button-text' => 'color: {{VALUE}};',
 				],
+			]
+		);
+
+		$this->add_control(
+			'ap_button_icon_hover_color',
+			[
+				'label'     => esc_html__( 'Icon Color', 'addon-pack' ),
+				'type'      => Controls_Manager::COLOR,
+				'selectors' => [
+					'{{WRAPPER}} .ap-button:hover i' => 'color: {{VALUE}};',
+				],
+				'condition' => [
+					'ap_button_icon!' => ''
+				]
 			]
 		);
 
@@ -323,8 +382,7 @@ class Button extends Widget_Base {
 			[
 				'name'      => 'ap_button_hover_background',
 				'types'     => [ 'classic', 'gradient' ],
-				'selector'  => '{{WRAPPER}} .ap-button:after, 
-								{{WRAPPER}} .ap-button:hover,',
+				'selector'  => '{{WRAPPER}} .ap-button:hover',
 			]
 		);
 
@@ -333,7 +391,7 @@ class Button extends Widget_Base {
 			[
 				'label'   => esc_html__( 'Border Style', 'addon-pack' ),
 				'type'    => Controls_Manager::SELECT,
-				'default' => 'solid',
+				'default' => 'none',
 				'options' => [
 					'none'   => esc_html__( 'None', 'addon-pack' ),
 					'solid'  => esc_html__( 'Solid', 'addon-pack' ),
@@ -354,12 +412,6 @@ class Button extends Widget_Base {
 				'label' => esc_html__( 'Border Size', 'addon-pack' ),
 				'type'  => Controls_Manager::DIMENSIONS,
 				'size_units' => [ 'px', '%' ],
-				'default' => [
-					'top'    => 3,
-					'right'  => 3,
-					'bottom' => 3,
-					'left'   => 3,
-				],
 				'selectors'  => [
 					'{{WRAPPER}} .ap-button:hover' => 'border-width: {{TOP}}{{UNIT}} {{RIGHT}}{{UNIT}} {{BOTTOM}}{{UNIT}} {{LEFT}}{{UNIT}};',
 				],
@@ -433,19 +485,14 @@ class Button extends Widget_Base {
 		$settings = $this->get_settings_for_display();
 		$button_icon_alignment = $settings['ap_button_icon_align'];
 
-		$this->add_render_attribute( 'ap_button', [
-			'class'	=> 'ap-button',
-			'href'	=> esc_attr($settings['ap_button_url']['url'] ),
-		]);
+		$button_link = '';
+        if($settings['ap_button_link_selection'] == 'existing_url' ) {
+            $button_link = get_permalink( $settings['ap_button_existing_url'] );
+        } elseif($settings['ap_button_link_selection'] == 'custom_url' ) {
+            $button_link = $settings['ap_button_url']['url'];
+        }
 
-		if( $settings['ap_button_url']['is_external'] ) {
-			$this->add_render_attribute( 'ap_button', 'target', '_blank' );
-		}
-		
-		if( $settings['ap_button_url']['nofollow'] ) {
-			$this->add_render_attribute( 'ap_button', 'rel', 'nofollow' );
-		}
-
+		$this->add_render_attribute( 'ap_button', 'class', 'ap-button');
 		$this->add_render_attribute( 'ap_button', 'class', esc_attr($settings['ap_button_text'] ));
 		$this->add_render_attribute( 'ap_button', 'class', 'ap-button-' . esc_attr($settings['ap_button_size']) );
 
@@ -457,8 +504,11 @@ class Button extends Widget_Base {
 
 		<div class="ap-button-wrapper">
 
-			<a <?php echo $this->get_render_attribute_string( 'ap_button' ); ?>>
-				<div class="ap-button-main">
+			<?php if( ! empty( $button_link ) ) : ?>
+				<a href="<?php echo esc_attr( $button_link ); ?>" <?php if( ! empty( $settings['ap_button_url']['is_external'] ) ) : ?> target="_blank" <?php endif; ?><?php if( ! empty( $settings['ap_button_url']['nofollow'] ) ) : ?> rel="nofollow" <?php endif; ?>>
+			<?php endif; ?>
+
+				<div <?php echo $this->get_render_attribute_string( 'ap_button' ); ?>>
 
 					<?php if($button_icon_alignment == 'left'): ?>
 						<i class="<?php echo esc_attr($settings['ap_button_icon'] ); ?> ap-button-icon-left" aria-hidden="true"></i> 
@@ -471,7 +521,9 @@ class Button extends Widget_Base {
 					<?php endif; ?>
 
 				</div>
-			</a>
+			<?php if( ! empty( $button_link ) ) : ?>
+				</a>
+			<?php endif; ?>
 
 		</div>
 
